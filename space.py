86882c2a4c46d827c1337469a76ad75943c319ba
gartemy <artemganzhaa@gmail.com>
2023-03-28 16:12:38 +0500

    Начало создания игры

---
+++ space.py
@@ -0,0 +1,71 @@
+import pygame
+from random import randint
+
+pygame.init()
+
+game_sc = pygame.display.set_mode([800, 600])
+icon = pygame.image.load('ufo.png')
+FPS = 60
+
+pygame.display.set_caption('Космические захватчики')
+pygame.display.set_icon(icon)
+
+clock = pygame.time.Clock()
+
+background = pygame.image.load('background.png')
+
+player = pygame.image.load('player.png')
+player_rect = player.get_rect()
+player_rect.x = 365
+player_rect.y = 500
+player_speed = 5
+
+enemies = []
+enemies_coords = []
+enemies_speed = []
+
+enemies_num = 5
+
+for i in range(enemies_num):
+    enemy = pygame.image.load('enemy.png')
+    enemies.append(enemy)
+    enemies_coords.append(pygame.Rect(
+        randint(0, 800 - enemy.get_rect().width), 
+        randint(50, 150),
+        enemy.get_rect().width,
+        enemy.get_rect().height
+    ))
+    enemies_speed.append(5)
+
+while True:
+    game_sc.fill(pygame.Color('black'))
+    game_sc.blit(background, [0, 0])
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player_rect.x -= player_speed
+        if player_rect.x < 0:
+            player_rect.x = 0
+    if keys[pygame.K_RIGHT]:
+        player_rect.x += player_speed
+        if player_rect.x > 800 - player_rect.width:
+            player_rect.x = 800 - player_rect.width
+    
+    for i in range(enemies_num):
+        enemy = enemies_coords[i]
+        enemy.x += enemies_speed[i]
+        if enemy.x > 800 - enemy.width:
+            enemy.y += 40
+            enemies_speed[i] = -enemies_speed[i]
+
+        game_sc.blit(enemies[i], enemies_coords[i])
+
+    game_sc.blit(player, player_rect)
+
+    pygame.display.flip()
+    clock.tick(FPS)
+
